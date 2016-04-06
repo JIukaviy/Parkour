fe755ef58186bfaaacda8c7340024b8c02ca67fb
JIukaviy-PC <dobryniny07@mail.ru>
2016-04-06 18:13:45 +1000

    Добавить функциональности в класс Queternion2D

--- Quaternion2D.cs
+++ Quaternion2D.cs
@@ -17,6 +17,11 @@ public class Quaternion2D {
         }
     }
 
+    public Quaternion2D() {
+        mCos = 1.0f;
+        mSin = 1.0f;
+    }
+
     public Quaternion2D(float x, float y) {
         float l = Mathf.Sqrt(x * x + y * y);
         mCos = x / l;
@@ -31,6 +36,11 @@ public class Quaternion2D {
         float radAngle = Mathf.Deg2Rad * Angle;
         mSin = Mathf.Sin(radAngle);
         mCos = Mathf.Cos(radAngle);
+    }
+
+    public Quaternion2D(Quaternion2D e) {
+        mCos = e.mCos;
+        mSin = e.mSin;
     }
 
     public Vector2 GetVector(float Length = 1) {
@@ -53,7 +63,53 @@ public class Quaternion2D {
         Rotate(new Quaternion2D(Angle));
     }
 
+    public void InverseRotate(Quaternion2D e) {
+        float newCos = mCos * e.mCos - mSin * e.mSin;
+        float newSin = -mCos * e.mSin - mSin * e.mCos;
+
+        mCos = newCos;
+        mSin = newSin;
+    }
+
+    public void InverseRotate(float Angle) {
+        InverseRotate(new Quaternion2D(Angle));
+    }
+
+    public void Inverse() {
+        mSin = -mSin;
+    }
+    
+    static public Quaternion2D Rotate(Quaternion2D Original, Quaternion2D Angle) {
+        Quaternion2D res = new Quaternion2D(Original);
+        res.Rotate(Angle);
+        return res;
+    } 
+
+    static public Quaternion2D InverseRotate(Quaternion2D Original, Quaternion2D Angle) {
+        Quaternion2D res = new Quaternion2D(Original);
+        res.InverseRotate(Angle);
+        return res;
+    }
+
+    static public Quaternion2D Inverse(Quaternion2D Original) {
+        Quaternion2D res = new Quaternion2D(Original);
+        res.Inverse();
+        return res;
+    }
+
     public static explicit operator Quaternion(Quaternion2D e) {
         return Quaternion.Euler(0, 0, e.angle);
+    }
+
+    public static Quaternion2D operator+(Quaternion2D a, Quaternion2D b) {
+        return Rotate(a, b);
+    }
+
+    public static Quaternion2D operator-(Quaternion2D a, Quaternion2D b) {
+        return InverseRotate(a, b);
+    }
+
+    public static Quaternion2D operator-(Quaternion2D a) {
+        return Inverse(a);
     }
 }
