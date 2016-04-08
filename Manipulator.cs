c59f62c6661a5fce9f953c9a783289b7fa35216f
JIukaviy-PC <dobryniny07@mail.ru>
2016-04-08 20:36:47 +1000

    Жалкие попытки прикрутить PID регулятор в Manipulator. Заменить углы Эйлера на Quaternion2D в Manipulator

--- Manipulator.cs
+++ Manipulator.cs
@@ -2,35 +2,58 @@
 using System.Collections;
 
 public class Manipulator : MonoBehaviour {
-    public float targetAngle;
+    Quaternion2D mTargetAngle;
     public float multiplier;
+    public float maxSpeed;
+    public float P;
+    public float I;
+    public float D;
+
     public float force {
-        get {
-            return mJoint.motor.maxMotorTorque;
-        }
+        get { return mJoint.motor.maxMotorTorque; }
         set {
             JointMotor2D motor = mJoint.motor;
             motor.maxMotorTorque = value;
             mJoint.motor = motor;
         }
     }
-    public float angle {
-        get {
-            return mJoint.jointAngle;
-        }
+
+    public Quaternion2D targetAngle {
+        get { return mTargetAngle.Clone(); }
+        set { mTargetAngle = value.Clone(); }
     }
 
-    private HingeJoint2D mJoint;
+    public Quaternion2D angle { get { return new Quaternion2D(-mJoint.jointAngle); } }
+
+    HingeJoint2D mJoint;
+
 
     public void Start() {
         mJoint = GetComponent<HingeJoint2D>();
-        targetAngle = mJoint.jointAngle;
-        multiplier = 1.0f;
+        mTargetAngle = new Quaternion2D(mJoint.jointAngle);
+        multiplier = 0.01f;
+        maxSpeed = 20;
+        force = 200;
+        
+        intErr = 0;
+        prevErr = 0;
+
+        P = 1f;
+        I = 0f;
+        D = 3;
     }
+    
+    float prevErr;
+    float intErr;
 
     void Update() {
         JointMotor2D motor = mJoint.motor;
-        motor.motorSpeed = (targetAngle - mJoint.jointAngle) * multiplier;
+        float Err = targetAngle / angle;
+        float dErr = Err - prevErr;
+        float U = P * Err + I * intErr + D * dErr * Time.deltaTime;
+        motor.motorSpeed = -Err * 2 - D * dErr * Time.deltaTime;
+        prevErr = Err;
+        intErr += Err / Time.deltaTime;
         mJoint.motor = motor;
     }
 }
