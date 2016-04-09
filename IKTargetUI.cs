a01a19a819a2c1262fb263a8c030eeebdb644847
JIukaviy-PC <dobryniny07@mail.ru>
2016-04-09 14:46:47 +1000

    Добавить скрипт управления UI Effector
    Управляет положением кнопки на экране, меняент положение эффектора в
    соответсвии с положением кнопки

---
+++ IKTargetUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class IKTargetUI : MonoBehaviour, IDragHandler {
+    IK.IKTarget mTarget;
+    RectTransform rectTransform;
+    Skeleton mSkeleton;
+
+    public IK.IKTarget IKTarget {
+        get { return mTarget; }
+        set {
+            if (mTarget != null) {
+                mTarget.OnPositionChange -= new EventHandler<IK.IKTarget.IKTargetEventArgs>(OnTargetPosChanged);
+            }
+            mTarget = value;
+            mTarget.OnPositionChange += new EventHandler<IK.IKTarget.IKTargetEventArgs>(OnTargetPosChanged);
+        }
+    }
+
+    public Skeleton skeleton {
+        get { return mSkeleton; }
+        set { mSkeleton = value; }
+    }
+
+    void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnTargetPosChanged(object sender, IK.IKTarget.IKTargetEventArgs e) {
+        rectTransform.position = Camera.main.WorldToScreenPoint(e.position);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mTarget.TryMoveTo(mTarget.position + Vector2.ClampMagnitude(worldPosition - mTarget.position, 0.1f));
+        mSkeleton.UpdateSkeleton();
+    }
+}
