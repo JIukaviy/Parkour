1d979f2fe9a71ad7b2e3fffda045f1624817117c
JIukaviy-PC <dobryniny07@mail.ru>
2016-04-14 23:00:59 +1000

    Добавить класс OnCollideInformer
    Вызывает callback если произошло столкновение с объектом с нужным слоем

---
+++ OnCollideInformer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+//Временный класс для тестирования
+public class OnCollideInformer : MonoBehaviour {
+    public EventHandler OnFinish;
+    public string LayerName;
+
+    int mLayer;
+
+    void Start() {
+        mLayer = LayerMask.NameToLayer(LayerName);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.gameObject.layer == mLayer && OnFinish != null) {
+            OnFinish.Invoke(this, new EventArgs());
+        }
+    }
+}
