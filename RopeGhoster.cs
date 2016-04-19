8117f8c1e39ed7de0c1d63f2471e3195c897d3c3
JIukaviy-PC <dobryniny07@mail.ru>
2016-04-20 02:28:20 +1000

    Добавить класс RopeGhoster
    Создает призрака веревки

---
+++ RopeGhoster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RopeGhoster : MonoBehaviour, GhostCreator.Ghoster {
+    GameObject[] mChainElements;
+
+    void Start() {
+        mChainElements = GetComponent<RopeCreator>().GetChainElements();
+        GhostCreator.RegisterGhoster(this);
+    }
+
+    public void CreateGhost() {
+        GameObject parent = null;
+
+        for (int i = 1; i < mChainElements.Length; i++) {
+            GameObject ghost = Instantiate(mChainElements[i]);
+            if (parent != null) {
+                ghost.GetComponent<HingeJoint2D>().connectedBody = parent.GetComponent<Rigidbody2D>();
+            }
+            GhostCreator.RegisterGhost(ghost, mChainElements[i]);
+            parent = ghost;
+        }
+    }
+}
